640038b7e9771b7544a11eb0e960e4cd4a006de8
Max Weller <max@teamwiki.de>
2015-07-07 02:39:11 +0200

    fix login header

--- views/core/login_info.php
+++ views/core/login_info.php
@@ -1,1 +1,1 @@
-        <li style=" color:#999;"><span class="glyphicon glyphicon-user"></span> <?= $name ? $name : $cn ?></li>
+        <li style=" color:#999;"><p class="navbar-text"><span class="glyphicon glyphicon-user"></span> <?= $name ? $name : $cn ?></p></li>
