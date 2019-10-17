b18ee11552dc56286f0cfbd5e483d711bb53f652
Max Weller <mweller@d120.de>
2019-10-18 01:00:39 +0200

    add feature to run predefined terminal commands from pad

--- private/js/pad_iframe.js
+++ private/js/pad_iframe.js
@@ -20,6 +20,47 @@ function export_popup(url) {
 	window.open(url, '', 'width=800,height=600,scrollbars=yes');
 	return false;
 }
+function loadScriptFile(u, c) {
+	  var d = document, t = 'script',
+		      o = d.createElement(t),
+		      s = d.getElementsByTagName(t)[0];
+	  o.src =  u;
+	  if (c) { o.addEventListener('load', function (e) { c(null, e); }, false); }
+	  s.parentNode.insertBefore(o, s);
+}
+var outTerm=null;
+function call_shell_cmd(cmd) {
+	$("#modal_export").modal("hide");
+	$("#modal_output").modal("show");
+	var xhr=null;
+	var idx=0;
+	var makeTerm=function() { console.log("maketerm called");
+			$("#terminal").html("");
+			outTerm = new Terminal();
+			outTerm.open(document.getElementById('terminal'));
+			//outTerm.write('Eile mit Weile ...\r\n')
+			if (xhr) {
+				outTerm.write(xhr.responseText.replace(/\n/g,"\r\n"));
+				idx=xhr.responseText.length;
+			}
+	}
+	if (typeof Terminal !== "undefined")
+			makeTerm();
+	else
+			loadScriptFile("js/xterm.js", makeTerm);
+	xhr=new XMLHttpRequest();
+	xhr.open("POST","?api",true);
+	xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+	xhr.send(cmd+"="+escape(padID));
+	xhr.onreadystatechange=function(e){
+		if(xhr.readyState==3 || xhr.readyState==4){
+			var newtext=xhr.responseText.substring(idx);
+			idx+=newtext.length;
+			if (outTerm) outTerm.write(newtext.replace(/\n/g,"\r\n"));
+			console.log(outTerm,newtext);
+		}
+	}
+}
 
 window.addEventListener("resize", on_resize, false);
 
